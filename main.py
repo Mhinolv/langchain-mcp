dee0452ee1396ecc8d9c94986956e8a87ba6e7de
Mhinoverse <Mhinolv@gmail.com>
2025-07-20 12:56:37 -0700

    Enhance main.py and update dependencies for MCP integration
    - Added ChatOpenAI and MCP tools to main.py for improved functionality.
    - Configured StdioServerParameters for the weather server.
    - Updated pyproject.toml to include isort as a dependency.
    - Modified uv.lock to reflect the addition of isort version 6.0.1.

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import asyncio
 import os
+
 from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters, stdio_server
+from mcp.client.stdio import stdio_client
 
 load_dotenv()
-print(os.getenv('OPENAI_API_KEY'))
+
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command = "python",
+    args = ["/Users/MarkHinojosa/mcp_servers/langchain-mcp/server/weather_server.py"],
+)
 
 async def main():
     print("Hello from langchain-mcp!")
-
 
 if __name__ == "__main__":
     asyncio.run(main())
